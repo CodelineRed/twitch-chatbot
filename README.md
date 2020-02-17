5ef9b2a6ef41cf1bbbb3e28ce20d00523d5105b8
InsanityMeetsHH <software@insanitymeetshh.net>
2020-02-17 19:43:23 +0100

    Setup project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# imhhbot
-This is a chat bot for Twitch
+# Twitch Chatbot - InsanityMeetsHH
+
+This application based on [Gulp Skeleton 5](https://github.com/InsanityMeetsHH/gulp-templating/tree/5.x).
+
+## Required
+* [Node.js](http://nodejs.org/en/download/) 8
+* [npm](http://www.npmjs.com/get-npm) `$ npm i npm@latest -g`
+
+## Quick start
+```bash
+$ git clone https://github.com/InsanityMeetsHH/twitch-chatbot.git
+$ cd [app-name]
+$ cp .env-dist .env
+Open .env and fill up all
+$ npm i
+$ node chatbot.js 
+```
+Open [localhost:3000](http://localhost:3000) for Web-GUI.
+Get `TWITCH_TOKEN` from [Twitch Chat OAuth Password Generator](https://twitchapps.com/tmi/).
+
+## Sources
+* [noopkat/twitch-count-chatbot](https://github.com/noopkat/twitch-count-chatbot)
+* [Glitch Twitch Chatbot](https://glitch.com/edit/#!/twitch-chatbot)
---
+++ chatbot.js
@@ -0,0 +1,65 @@
+const tmi        = require('tmi.js');
+const skateboard = require('skateboard');
+require('dotenv').config();
+
+let count = 0;
+let socket = null;
+
+// Define configuration options
+const opts = {
+    identity: {
+        username: process.env.TWITCH_NICK,
+        password: process.env.TWITCH_TOKEN
+    },
+    channels: [
+        process.env.TWITCH_CHANNEL
+    ]
+};
+
+// Create a client with options
+const client = new tmi.client(opts);
+
+// Register event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+    if (self) { return; } // Ignore messages from the bot
+
+    // Remove whitespace from chat message
+    const cmdName = msg.trim();
+
+    // If the command is known, let's execute it
+    if (cmdName === '!d20') {
+        const num = rollDice(cmdName);
+        client.say(target, `You rolled a ${num}.`);
+        console.log(`* Executed ${cmdName} command`);
+    } else {
+        console.log(`* Unknown command ${cmdName}`);
+    }
+    
+    if (/^\d\d?$/.test(cmdName)) {
+        const n = parseInt(cmdName);
+        count = (n - count === 1) ? n : 0;
+        socket.write(count.toString()); 
+    }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+    const sides = 20;
+    return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+    
+    skateboard({port: 3000}, (stream) => {
+        socket = stream;
+    });
+}
