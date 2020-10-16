c204cd5e6698d8d7826c6ff106f8857d4dea0a83
InsanityMeetsHH <software@insanitymeetshh.net>
2020-10-16 17:41:39 +0200

    Production build

--- public/js/vue/component/layout/empty.js
+++ public/js/vue/component/layout/empty.js
@@ -1,1 +1,1 @@
-define(["exports"],(function(e){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;e.default={template:'<div class="container-fluid"><router-view ref="view"></router-view></div>'}}));
+define(["exports"],(function(e){"use strict";Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0;e.default={template:'<div class="container-fluid"><router-view ref="view" /></div>'}}));
